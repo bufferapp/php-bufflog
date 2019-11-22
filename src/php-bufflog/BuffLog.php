685a25a3545ebe4d6f9d34d77dae420dd7b092b8
Eric Khun <enregistrements@gmail.com>
2019-11-22 05:50:51 +0000

    formatting messages

--- src/php-bufflog/BuffLog.php
+++ src/php-bufflog/BuffLog.php
@@ -23,28 +23,45 @@ class BuffLog {
 
     public function debug($message)
     {
+        $this->formatMessage($message, Logger::DEBUG, $context = [], $extra = []);
         $this->getLogger()->debug($message);
     }
 
     public function info($message)
     {
+        $this->formatMessage($message, Logger::INFO, $context = [], $extra = []);
         $this->getLogger()->info($message);
     }
 
     public function warn($message)
     {
+        $this->formatMessage($message, Logger::WARNING, $context = [], $extra = []);
         $this->getLogger()->warn($message);
     }
 
 
     public function error($message)
     {
+        $this->formatMessage($message, Logger::ERROR, $context = [], $extra = []);
         $this->getLogger()->error($message);
     }
 
+    // @TODO: That one might could also create an alert in Datadog?
     public function critical($message)
     {
+        $this->formatMessage($message, Logger::CRITICAL, $context = [], $extra = []);
         $this->getLogger()->critical($message);
+    }
+
+    private function formatMessage($message, $level, $context = [], $extra = [])
+    {
+        $output = [
+            "message"   => $message,
+            "level"     => $level,
+            "datetime"  => date(\DateTime::ATOM),
+            "context"   => $context,
+            "extra"     => $extra
+        ];
     }
 
     private function createLogger()
