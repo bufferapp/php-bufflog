ebd7206a3c5b2308a5cad3f37cdc44a09b03e50b
Eric Khun <enregistrements@gmail.com>
2019-11-22 05:30:10 +0000

    create logger

--- src/php-bufflog/BuffLog.php
+++ src/php-bufflog/BuffLog.php
@@ -1,6 +1,32 @@
 <?php
 namespace Buffer;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler as MonologStreamHandler;
+
+/*
+    Level of logs we use:
+
+    This level require manual action to appear in Datadog Logs
+    Logger::DEBUG
+    Logger::INFO
+
+    Everything at this level appears by default in Datadog Logs
+    Logger::WARNING
+    Logger::ERROR
+    Logger::CRITICAL
+*/
+
 class BuffLog {
+
+    protected function createLogger()
+    {
+        $logger = new Logger('php-bufflog');
+        $handler = new MonologStreamHandler('php://stdout', Logger::INFO);
+
+        $logger->pushHandler($handler);
+
+        return $logger;
+    }
 
 }
